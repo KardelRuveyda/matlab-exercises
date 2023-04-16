ab01f4e5ef54163d7dc8b5b1fe9ae2432f19ac35
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2023-04-17 02:00:20 +0300

    matlab exercises and exam prep

---
+++ w5/struct_script.m
@@ -0,0 +1,7 @@
+clc
+clear 
+ogrenci.ad ="Kardel";
+ogrenci.no = 135;
+ogrenci.sinif=2;
+ogrenci
+whos
--- w7/plot_hw_w7.m
+++ w7/plot_hw_w7.m
@@ -11,29 +11,28 @@ elseif nargin == 3
     N=100;
 end
 
-%%vektör olarak verdim.
+% vektör olarak verdim.
 result = compute_int(f,a,b,N);
 
 if nargout == 2
-res_2 = compute_int(f,a,b,N-1);
-epsilon = abs(result-res_2);
+    res_2 = compute_int(f,a,b,N-1);
+    epsilon = abs(result-res_2);
 end
 
-% grafik çizimi
-x = linspace(a,b,N);
+delta = (b-a)/(N-1);
+
+x = linspace(a, b, N);
 y = f(x);
-plot(x,y);
+
+plot(x, y);
 xlabel('x');
 ylabel('f(x)');
-title('f(x) Grafiği');
+title('Sonuc:',result);
 
 end
 
 function [r] = compute_int(f,a,b,N)
-delta = (b-a)/(N-1);
-X = linspace(a,b,N);
-r = delta * (sum(f(X(2:N-1))) + (f(X(1))+f(X(N)))/2);
+    delta = (b-a)/(N-1);
+    X = linspace(a,b,N);
+    r = delta * (sum(f(X(2:N-1))) + (f(X(1))+f(X(N)))/2);
 end
-
-% g=@(x) 2*x.^2-1;
-% plot_hw_w7(g,0,4,50)
